78aaa6d851f87e58a0e5e87749a56fd763be68a3
Michal Bocek <michal.bocek@embedit.cz>
2021-11-17 21:24:12 +0100

    feat(database): added support for postgres

--- src/test/java/org/evidence/AbstractIntegrationTest.java
+++ src/test/java/org/evidence/AbstractIntegrationTest.java
@@ -1,8 +1,10 @@
 package org.evidence;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = {ComputerApplication.class, H2TestProfileJPAConfig.class},
                 webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class AbstractIntegrationTest {
 }
